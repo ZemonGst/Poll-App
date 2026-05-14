9137d9a922a0d0e0a816b8beeb270e8ebeb1aab1
Soumyaditya Sinha <sinhasoumyaditya60@gmail.com>
2026-05-15 03:12:50 +0530

    feat: configure Google OAuth20 strategy with Passport.js

--- server/src/common/config/passport.js
+++ server/src/common/config/passport.js
@@ -1,47 +1,19 @@
 import passport from "passport";
-
-import {
-  Strategy as GoogleStrategy,
-} from "passport-google-oauth20";
-
-import {
-  findOrCreateGoogleUser,
-} from "../../modules/auth/services/authService.js";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import { findOrCreateGoogleUser } from "../../modules/auth/services/authService.js";
 
 passport.use(
-
   new GoogleStrategy(
-
     {
-
-      clientID:
-        process.env.GOOGLE_CLIENT_ID,
-
-      clientSecret:
-        process.env.GOOGLE_CLIENT_SECRET,
-
-      callbackURL:
-        "/api/auth/google/callback",
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
     },
-
-    async (
-      accessToken,
-      refreshToken,
-      profile,
-      done
-    ) => {
-
+    async (accessToken, refreshToken, profile, done) => {
       try {
-
-        const user =
-          await findOrCreateGoogleUser(
-            profile
-          );
-
+        const user = await findOrCreateGoogleUser(profile);
         return done(null, user);
-
       } catch (error) {
-
         return done(error, null);
       }
     }
